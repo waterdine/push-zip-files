08d2f4a51dd2b96e1c7d48f2e9db4508d2c06f76
x414e54 <x414e54@linux.com>
2020-11-08 16:00:34 +0000

    Update index.js
    Use execSync instead.

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const github = require('@actions/github');
 
 try {
   const repo = core.getInput('repo');
-  const { exec, spawn } = require('child_process');
+  const { execSync, spawn } = require('child_process');
   
   if (repo.includes('\'') || repo.includes('"'))
   {
      core.setFailed("Invalid characters in string");
   }
   
-  exec('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" add *.zip', (err, stdout, stderr) => {
+  execSync('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" add *.zip', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     if (err) {
@@ -20,7 +20,7 @@ try {
     }
   });
   
-  exec('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" commit -m "Update binaries"', (err, stdout, stderr) => {
+  execSync('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" commit -m "Update binaries"', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     if (err) {
@@ -29,7 +29,7 @@ try {
     }
   });
   
-  exec('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" push origin main', (err, stdout, stderr) => {
+  execSync('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" push origin main', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     if (err) {
