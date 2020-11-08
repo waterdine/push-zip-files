5c15dce8adb4b2d94f1962b38ef2cb83f1b06780
x414e54 <x414e54@linux.com>
2020-11-08 16:07:14 +0000

    Update index.js
    Add username and email settings to javascript.

--- index.js
+++ index.js
@@ -4,12 +4,32 @@ const github = require('@actions/github');
 
 try {
   const repo = core.getInput('repo');
+  const username = core.getInput('username');
+  const email = core.getInput('email');
   const { execSync, spawn } = require('child_process');
   
-  if (repo.includes('\'') || repo.includes('"'))
+  if (repo.includes('\'') || repo.includes('"') || username.includes('\'') || username.includes('"') || email.includes('\'') || email.includes('"'))
   {
      core.setFailed("Invalid characters in string");
   }
+  
+  execSync('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" config user.email "' + email + '"', (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (err) {
+      core.setFailed(err);
+      return;
+    }
+  });
+  
+  execSync('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" config user.name "' + username + '"', (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (err) {
+      core.setFailed(err);
+      return;
+    }
+  });
   
   execSync('"C:\\Program Files\\Git\\cmd\\git.exe" -C "' + repo + '" add *.zip', (err, stdout, stderr) => {
     console.log(stdout);
